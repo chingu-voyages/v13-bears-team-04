2d4d54d1028cd29934e052d0bac07c14ec25d373
Daniel Strong <danielstrong.ds@gmail.com>
2019-11-15 19:39:08 +0900

    changed app port to accommadate api and added env var for dev api url

--- client/next.config.js
+++ client/next.config.js
@@ -1,2 +1,7 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const withSass = require("@zeit/next-sass")
+
+module.exports = withSass({
+  env: {
+    API_URL_DEV: "http://localhost:3000",
+  },
+})

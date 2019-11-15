03cbde494832c5d01859dd7796613dd14e669d2a
Damian <d.pagowski@gmail.com>
2019-11-15 23:03:13 +0700

    removed bunyan logger

--- api-server/app.js
+++ api-server/app.js
@@ -2,13 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const postsRouter = require('./routes/posts')
-
+const morgan = require('morgan')
+const morganBody = require('morgan-body')
 const DB_URI = process.env.MONGOLAB_URI
 const APP_PORT = process.env.PORT || 3000
 
 mongoose.connect(DB_URI, { useNewUrlParser: true })
 
 const app = express()
+morganBody(app);
+morgan('combined')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))

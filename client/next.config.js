d09c03bb0be4d1d2370f31a6bb5966ce31788f4b
Daniel Strong <danielstrong.ds@gmail.com>
2019-11-16 10:18:19 +0900

    adjusted env structure

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,8 @@
 const withSass = require("@zeit/next-sass")
+require("dotenv").config()
 
 module.exports = withSass({
   env: {
-    API_URL_DEV: "http://localhost:3000",
+    API_URL: process.env.API_URL,
   },
 })

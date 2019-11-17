665fd004f4c68682c33691f996d17cca5fa8f47e
Daniel Strong <danielstrong.ds@gmail.com>
2019-11-17 17:15:09 +0900

    organized file

--- api-server/app.js
+++ api-server/app.js
@@ -1,33 +1,44 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const postsRouter = require('./routes/posts')
-const morgan = require('morgan')
-const morganBody = require('morgan-body')
-const DB_URI = process.env.MONGOLAB_URI
-const APP_PORT = process.env.PORT || 3000
-const rateLimit = require('express-rate-limit')
-const cors = require('cors');
+require("dotenv").config();
 
+const express = require("express");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+const morganBody = require("morgan-body");
+const mongoose = require("mongoose");
+
+const app = express();
+const DB_URI = process.env.MONGOLAB_URI;
+const APP_PORT = process.env.PORT || 3000;
+
+// MONGO CONNECTION
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch(error => console.log("Database error: " + JSON.stringify(error)));
+
+// https://www.npmjs.com/package/express-rate-limit#usage
+app.set("trust proxy", 1);
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100
-})
+});
 
-mongoose.connect(DB_URI, { useNewUrlParser: true }).catch(error => console.log('Cannot connect to database ' + JSON.stringify(error)))
-
-const app = express()
-morganBody(app)
-morgan('combined')
-app.use(limiter)
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// MIDDLEWARE
 app.use(cors());
+app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-postsRouter(app)
-app
-    .route('/')
-    .get((req, res) => res.json({status: "OK"}));
+// https://www.npmjs.com/package/morgan-body
+morganBody(app);
+morgan("combined");
 
-app.listen(APP_PORT, () => console.log('Listening on port ' + APP_PORT))
-module.exports = app
+// API ROUTES
+const routes = require("./routes");
+app.use("/api", routes);
+
+app.get("/", (_, res) => res.status(200).json({ status: "OK" }));
+
+app.listen(APP_PORT, () => console.log("Listening on port " + APP_PORT));
+
+module.exports = app;

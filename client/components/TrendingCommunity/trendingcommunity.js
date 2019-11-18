711f0e02867d1773f0636ec34657a48b93a5b197
Saggitarie <saggitarie@gmail.com>
2019-11-18 18:31:25 +0900

    Display the list of trending communities by iterating them through map function. At the current moment, the displayed data are all dummy.
    Todos:
    1) Retrieve the top five trends using HTTP requests
    2) Design the component using CSS properties.

---
+++ client/components/TrendingCommunity/trendingcommunity.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import "./trendingcommunity.scss";
+
+export default function trendingcommunity() {
+  const [subReddits, setTrendingSubReddit] = useState([
+    { id: 1, name: "r/number1", numOfMembers: 27000 },
+    { id: 2, name: "r/number2", numOfMembers: 56000 },
+    { id: 3, name: "r/number3", numOfMembers: 75000 },
+    { id: 4, name: "r/number4", numOfMembers: 50000 },
+    { id: 5, name: "r/number5", numOfMembers: 20000 },
+  ]);
+  return subReddits.map((subReddit, index) => {
+    return (
+      <div>
+        <p>{subReddit.name}</p>
+        <p>{subReddit.numOfMembers}</p>
+        {/* eslint-disable-next-line react/button-has-type */}
+        <button>Join</button>
+      </div>
+    );
+  });
+}
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "../components/nav";
+import TrendingCommunity from "../components/TrendingCommunity/trendingcommunity";
 import PostList from "../components/postlist";
 
 const Home = () => (
   <div>
     <Nav />
     <PostList />
+    <TrendingCommunity />
   </div>
 );
-
 export default Home;

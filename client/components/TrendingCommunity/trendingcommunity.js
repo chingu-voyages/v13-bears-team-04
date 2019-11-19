af26926c855450d1a90315cbb18e78173b27690e
Saggitarie <saggitarie@gmail.com>
2019-11-19 17:29:57 +0900

    Styled components (expect for the join button since we are going to make it consitent throughout the application by creating the  reusable button.)

--- client/components/TrendingCommunity/trendingcommunity.js
+++ client/components/TrendingCommunity/trendingcommunity.js
@@ -4,26 +4,48 @@ import "./trendingcommunity.scss";
 
 export default function trendingcommunity() {
   const [subReddits, setTrendingSubReddit] = useState([
-    { id: 1, name: "r/number1", numOfMembers: 27000 },
-    { id: 2, name: "r/number2", numOfMembers: 56000 },
-    { id: 3, name: "r/number3", numOfMembers: 75000 },
-    { id: 4, name: "r/number4", numOfMembers: 50000 },
-    { id: 5, name: "r/number5", numOfMembers: 20000 },
+    { id: 1, name: "r/climate-change", numOfMembers: 27000 },
+    { id: 2, name: "r/fashion", numOfMembers: 56000 },
+    { id: 3, name: "r/social", numOfMembers: 75000 },
+    { id: 4, name: "r/politics", numOfMembers: 50000 },
+    { id: 5, name: "r/travel", numOfMembers: 20000 },
   ]);
-  return subReddits.map((subReddit, index) => {
-    return (
-      <div className="trending-community">
-        <div className="row">
-          <div className="col-2-of-3">
-            <p>{subReddit.name}</p>
-            <p>{`${subReddit.numOfMembers} k members`}</p>
+  return (
+    <div className="trending-community">
+      <p className="trending-community__headline">Trending communities</p>
+      {subReddits.map((subReddit, index) => {
+        return (
+          <div>
+            <div className="row">
+              <div className="col-1-of-4">
+                <div className="image-wrapper">
+                  <img
+                    className="icon"
+                    src={`/trending-images/icon-${index + 1}.jpg`}
+                    alt="photo1"
+                  />
+                </div>
+              </div>
+              <div className="col-2-of-4">
+                <div className="subreddit-info">
+                  <p className="trending-community__subreddit-info__sub-info-1">
+                    {subReddit.name}
+                  </p>
+                  <p className="trending-community__subreddit-info__sub-info-2">
+                    {`${subReddit.numOfMembers} k members`}
+                  </p>
+                </div>
+              </div>
+              <div className="col-1-of-4">
+                <div className="trending-community__join-btn">
+                  {/* eslint-disable-next-line react/button-has-type */}
+                  <button>Join</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-1-of-3">
-            {/* eslint-disable-next-line react/button-has-type */}
-            <button>Join</button>
-          </div>
-        </div>
-      </div>
-    );
-  });
+        );
+      })}
+    </div>
+  );
 }

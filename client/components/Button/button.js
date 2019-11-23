324c1bde46f105666a1bef683aca1e75edaaca86
Saggitarie <saggitarie@gmail.com>
2019-11-24 00:59:35 +0900

    Set the className (Css Property) on the parent div element. I also made corresponding adjustment in CSS styling according to these changes.

--- client/components/Button/button.js
+++ client/components/Button/button.js
@@ -3,19 +3,16 @@ import "./button.scss";
 
 export default function button(props) {
   return (
-    <div>
-      {/* eslint-disable-next-line react/destructuring-assignment,jsx-a11y/anchor-is-valid */}
-      <a
-        href="#"
-        /* eslint-disable-next-line react/destructuring-assignment,jsx-a11y/anchor-is-valid */
-        className={`btn btn--${props.color} btn--${props.color}--${
-          // eslint-disable-next-line react/destructuring-assignment
-          props.inverted === false ? "inverse" : "reverse"
-        }`}
-      >
-        {/* eslint-disable-next-line react/destructuring-assignment */}
-        {props.children}
-      </a>
+    // eslint-disable-next-line react/destructuring-assignment
+    <div
+      /* eslint-disable-next-line react/destructuring-assignment */
+      className={`btn btn--${props.color} btn--${props.color}--${
+        // eslint-disable-next-line react/destructuring-assignment
+        props.inverted === false ? "inverse" : "reverse"
+      }`}
+    >
+      {/* eslint-disable-next-line react/destructuring-assignment */}
+      {props.children}
     </div>
   );
 }

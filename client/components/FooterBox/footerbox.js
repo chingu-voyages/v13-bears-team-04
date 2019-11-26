335373bf4e4769f54e4df6745bb9410049d1ad46
beboptank <beboptank12@gmail.com>
2019-11-26 23:41:05 +0900

    add component/skeleton for footerbox

---
+++ client/components/FooterBox/footerbox.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./footerbox.scss";
+
+export default function FooterBox() {
+  <div>
+    <div>
+      <ul>
+        Reddit
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Careers</a>
+        </li>
+        <li>
+          <a href="#">Press</a>
+        </li>
+        <li>
+          <a href="#">Advertise</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        Using Reddit
+        <li>
+          <a href="#">Help</a>
+        </li>
+        <li>
+          <a href="#">Reddit App</a>
+        </li>
+        <li>
+          <a href="#">Reddit Coins</a>
+        </li>
+        <li>
+          <a href="#">Reddit Premium</a>
+        </li>
+        <li>
+          <a href="#">Reddit Gifts</a>
+        </li>
+        <li>
+          <a href="#">Communities</a>
+        </li>
+        <li>
+          <a href="#">Top Posts</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <p>
+        <a href="#">Terms</a> | <a href="#">Content Policy</a> |{" "}
+        <a href="#">Privacy Policy</a> | <a href="#">Mod Policy</a>
+      </p>
+    </div>
+    <div>
+      <p>Definitely Not Reddit Inc &copy; 2019</p>
+    </div>
+  </div>;
+}

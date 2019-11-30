5ec95e161e04b252d219f563050c184c44eda63f
beboptank <beboptank12@gmail.com>
2019-11-30 10:57:06 +0900

    changes to placement of navitems using grid

--- client/components/Navigation/navigation.js
+++ client/components/Navigation/navigation.js
@@ -6,32 +6,42 @@ const Nav = () => (
   <nav className="row">
     <div>
       <ul className="mainnav">
-        <li className="navitem">
-          <a>
-            <img></img>
-          </a>
-        </li>
-        <li className="navitem">
-          <select>
-            <option value="filter">
-              <input value="Filter" type="text" />
-            </option>
-            <option value="popular"></option>
-            <option value="all"></option>
-            <option value="topCommmunities"></option>
-            <option value="coins"></option>
-            <option value="premium"></option>
-          </select>
-        </li>
-        <li className="navitem">
-          <input type="text" value="Search Reddit" name="searchReddit" />
-        </li>
-        <li className="navitem">
-          <button>Popular</button>
-        </li>
-        <li className="navitem">
-          <button>All</button>
-        </li>
+        <div className="col-1-of-4">
+          <li className="navitem">
+            <a>
+              <img></img>
+            </a>
+          </li>
+        </div>
+        <div class="col-2-of-4">
+          <li className="navitem">
+            <select>
+              <option value="filter">
+                <input value="Filter" type="text" />
+              </option>
+              <option value="popular"></option>
+              <option value="all"></option>
+              <option value="topCommmunities"></option>
+              <option value="coins"></option>
+              <option value="premium"></option>
+            </select>
+          </li>
+        </div>
+        <div class="col-3-of-4">
+          <li className="navitem">
+            <input type="text" value="Search Reddit" name="searchReddit" />
+          </li>
+        </div>
+        <div class="col-4-of-4">
+          <li className="navitem">
+            <button>Popular</button>
+          </li>
+        </div>
+        <div className="col-4-of-4">
+          <li className="navitem">
+            <button>All</button>
+          </li>
+        </div>
         <div className="col-4-of-4">
           <li className="navitem">
             <Button color="blue">Login</Button>
@@ -44,16 +54,18 @@ const Nav = () => (
             </Button>
           </li>
         </div>
-        <li className="navitem">
-          <select>
-            <option>Icon</option>
-            <option>Night Mode</option>
-            <option>Reddit Coins</option>
-            <option>Reddit Premium</option>
-            <option>Help Center</option>
-            <option>Log In/Sign Up</option>
-          </select>
-        </li>
+        <div className="col-4-of-4">
+          <li className="navitem">
+            <select>
+              <option>Icon</option>
+              <option>Night Mode</option>
+              <option>Reddit Coins</option>
+              <option>Reddit Premium</option>
+              <option>Help Center</option>
+              <option>Log In/Sign Up</option>
+            </select>
+          </li>
+        </div>
       </ul>
     </div>
   </nav>

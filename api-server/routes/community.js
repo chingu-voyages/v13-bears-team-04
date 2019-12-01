1818b0070180b38f90eb5dba500cd1861d493dab
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-01 14:03:20 +0900

    init community routes commit

---
+++ api-server/routes/community.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+
+module.exports = router;
--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const posts = require("./posts");
 const users = require("./users");
+const community = require("./community");
 
 router.use("/user", users);
 router.use("/posts", posts);
+router.use("/community", community);
 
 module.exports = router;

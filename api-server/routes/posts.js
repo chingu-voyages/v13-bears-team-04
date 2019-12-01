b25b6c32539ad0d5f6290bf62f971ea328646cb1
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-01 14:35:02 +0900

    fixed status code again

--- api-server/routes/posts.js
+++ api-server/routes/posts.js
@@ -35,7 +35,7 @@ async function getCommunityPosts(req, res) {
 async function createPost(req, res) {
   try {
     const newPost = await Post.create(req.body);
-    res.status(200).json(newPost);
+    res.status(201).json(newPost);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);

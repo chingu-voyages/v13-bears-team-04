e3fa9efede860240175b5b4ce9a64ee51c000dea
Saggitarie <saggitarie@gmail.com>
2019-12-01 09:02:30 +0900

    WIP: Working on popup custom form component.

--- client/components/Form/signin.js
+++ client/components/Form/signin.js
@@ -10,24 +10,35 @@ export default function signin() {
     <div className="form__popup">
       <div className="form__content">
         <form action="#" className="form__main">
-          <div className="form__main__left">
+          <div className="form__left">
             <img
               src="form_image.png"
               alt="reddit_sub_icon"
-              className="form__main__left__image"
+              className="form__left__image"
             />
           </div>
-          <div className="form__main__right">
+          <div className="form__right">
             <div className="form__wrapper">
               <div className="form__wrapper__icon">
-                <img src="reddit_icon.png" alt="reddit_sub_icon" />
+                <img
+                  src="reddit_icon.png"
+                  alt="reddit_sub_icon"
+                  className="reddit-icon"
+                />
               </div>
               <div className="form__wrapper__title">Sign in</div>
-              <Input>Username</Input>
-              <Input>Password</Input>
-              <Button color="blue" inverted={false}>
-                Sign In
-              </Button>
+              <div className="form__wrapper__input">
+                <Input>Username</Input>
+              </div>
+              <div className="form__wrapper__input">
+                <Input>Password</Input>
+              </div>
+
+              <div className="form__wrapper__button">
+                <Button color="blue" inverted={false}>
+                  Sign In
+                </Button>
+              </div>
             </div>
           </div>
         </form>

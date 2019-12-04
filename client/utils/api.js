30c439aa232e50d13bd871d06580249d64ca4a22
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-04 21:06:42 +0900

    updated custom fetch

--- client/utils/api.js
+++ client/utils/api.js
@@ -1,10 +1,33 @@
-export async function customFetch(url, options) {
+import fetch from "isomorphic-unfetch";
+import { parseCookies } from "nookies";
+
+export default async function fetchIt(
+  url,
+  givenOptions = {},
+  isAuthNeeded = false
+) {
+  const options = makeOptions(givenOptions, isAuthNeeded);
   // typical fetch
   const resp = await fetch(`${process.env.API_URL}${url}`, options);
-  // parse the response as usual
+  // parse the response
   const data = await resp.json();
-  // if there's an error, throw that message
-  if (!resp.ok) throw data.error;
+  // if there's was an error, throw it
+  if (!resp.ok) throw data;
   // otherwise return the results
   return data;
+}
+
+function makeOptions({ method = "GET", body }, isAuthNeeded) {
+  const auth = isAuthNeeded ? getSessionId() : {};
+  return {
+    method,
+    body,
+    headers: { "Content-Type": "application/json", ...auth },
+  };
+}
+
+function getSessionId() {
+  const { sid } = parseCookies();
+  if (!sid) throw new Error("Sorry, no session cookie found");
+  return { Authorization: JSON.stringify({ sid }) };
 }

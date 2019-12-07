b7a1f55bab48077cc4aa141520932251ab62aed4
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-07 13:57:13 +0900

    edited/completed checkSession middlware

--- api-server/middleware/index.js
+++ api-server/middleware/index.js
@@ -1,14 +1,30 @@
+const createError = require("http-errors");
 const Session = require("../models/session");
-// const User = require("../models/user");
+const User = require("../models/user");
 
 exports.checkSession = async function(req, res, next) {
   try {
-    const { sid } = req.cookies;
-    const session = await Session.findOne({ _id: sid });
-    if (session) next();
-    res.status(401).json({ message: "Please log in first" });
+    // check for a valid session
+    if (!("authorization" in req.headers)) {
+      throw createError(
+        401,
+        "You messed up, the authorization header is missing"
+      );
+    }
+    const { sid } = JSON.parse(req.headers.authorization);
+    const session = await Session.findById(sid);
+    if (!session) {
+      throw createError(401, "Invalid session. Please log in again.");
+    }
+    // grab the session's user object and pass it along if valid
+    const user = await User.findById(session.userId);
+    if (!user) {
+      throw createError(401, "Valid session, but couldn't find the user");
+    }
+    // sets the user for this request, so you can use route function
+    res.locals.user = user;
+    next();
   } catch (err) {
-    console.log(err);
-    res.status(401).json({ message: "Sorry, an error occured" });
+    next(err);
   }
 };

3c7d200245f86c719adf18f9fb2d1b81cea07a3c
Saggitarie <saggitarie@gmail.com>
2019-12-09 00:21:51 +0900

    Customized the dropdown indicator and adjust select item size.

--- client/components/SortView/sortview.js
+++ client/components/SortView/sortview.js
@@ -1,12 +1,53 @@
 import React from "react";
-import Select from "react-select";
+import Select, { components } from "react-select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
+import { sortOptions } from "./data/data";
+import { countryOptions } from "./data/data";
+
+import "./sortview.scss";
+
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: "1px dotted #d7d7d7",
+    color: state.isSelected ? "#5296dd" : "#d7d7d7",
+    width: state.selectProps.width,
+    backgroundColor: state.isSelected ? "#ffffff" : "#ffffff",
+    padding: 10,
+    fontSize: 17,
+  }),
+  menu: (provided, state) => ({
+    ...provided,
+    width: state.selectProps.width,
+    //   borderBottom: "1px dotted pink",
+    //   padding: 20,
+  }),
+};
+
+const DropdownIndicator = props => {
+  return (
+    components.DropdownIndicator && (
+      <components.DropdownIndicator {...props}>
+        <FontAwesomeIcon
+          icon={props.selectProps.menuIsOpen ? "caret-up" : "caret-down"}
+        />
+      </components.DropdownIndicator>
+    )
+  );
+};
 
 export default function sortview() {
-  return <Select options={options} />;
+  return (
+    <div className="sortview">
+      <Select
+        defaultValue={sortOptions[0]}
+        options={sortOptions}
+        label="Single Select"
+        components={{ DropdownIndicator }}
+        styles={customStyles}
+        width="100%"
+      />
+    </div>
+  );
 }

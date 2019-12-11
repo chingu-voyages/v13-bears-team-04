bc3d061b6d2caa1a550e7c93bc70886486a5f701
Saggitarie <saggitarie@gmail.com>
2019-12-11 16:10:48 +0900

    Create component for view section

---
+++ client/components/SortView/view.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function view() {
+  return (
+    <div className="view">
+      <h2>View</h2>
+      <img className="view__img" src="grid-images/small-grid.png" />
+      <img className="view__img" src="grid-images/medium-grid.png" />
+      <img className="view__img" src="grid-images/large-grid.png" />
+    </div>
+  );
+}

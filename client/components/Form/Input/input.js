fad8996402edb2d45d4ea65fd45f64668b4500ed
Saggitarie <saggitarie@gmail.com>
2019-12-11 19:53:20 +0900

    Set input type and value dynammically depending on props.children.

--- client/components/Form/Input/input.js
+++ client/components/Form/Input/input.js
@@ -12,15 +12,30 @@ export default function input(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  let inputType = props.children;
+  let type, inputValue;
+
+  if (inputType.toLowerCase() === "email") {
+    type = "email";
+    inputValue = email;
+  } else if (inputType.toLowerCase() === "username") {
+    type = "text";
+    inputValue = username;
+  } else {
+    type = "password";
+    inputValue = password;
+  }
+
   return (
     <div className="form__container form__wrapper__input">
       <input
         className="form__input"
-        type="text"
+        type={type}
         placeholder=""
         id="form__placeholder"
         required
         pattern="\S+.*"
+        value={inputValue}
       />
       <label className="form__placeholder" htmlFor="form__placeholder">
         {props.children}

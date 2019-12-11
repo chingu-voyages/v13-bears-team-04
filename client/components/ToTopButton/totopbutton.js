7b992558b1575eb0753023028587d6d372956989
beboptank <beboptank12@gmail.com>
2019-12-11 20:28:48 +0900

    Refactored to use new Button component and better CSS styling

--- client/components/ToTopButton/totopbutton.js
+++ client/components/ToTopButton/totopbutton.js
@@ -3,7 +3,7 @@ import Button from "../Button/button";
 import "./totopbutton.scss";
 
 export default function ToTopButton() {
-  const [top, resetYPosition] = useState(0);
+  const [top] = useState(0);
 
   function scrollToTop() {
     if (window.scrollY > 0) {
@@ -16,13 +16,12 @@ export default function ToTopButton() {
 
   return (
     <div className="row">
-      <div className="to_top_container">
-        {/* eslint-disable-next-line react/button-has-type */}
-        <Button>
-          <button className="to_top_button" onClick={scrollToTop}>
-            Scroll To Top
-          </button>
-        </Button>
+      <div className="totop--container">
+        <Button
+          text="Scroll To Top"
+          color="blue"
+          handleClick={scrollToTop}
+        ></Button>
       </div>
     </div>
   );

0da594d6709ef2685f0eef5f9371d3c855cdd8cf
unknown <danielstrong.ds@gmail.com>
2019-12-13 16:33:25 +0900

    changed container css naming

--- client/components/SortView/sortview.js
+++ client/components/SortView/sortview.js
@@ -1,14 +1,12 @@
 import React from "react";
-
 import CountryOptions from "./countryoptions";
 import Sort from "./sort";
 import View from "./view";
-
 import "./sortview.scss";
 
 export default function SortView() {
   return (
-    <div className="sortview__container">
+    <div className="sortview-container">
       <View />
       <Sort />
       <CountryOptions />

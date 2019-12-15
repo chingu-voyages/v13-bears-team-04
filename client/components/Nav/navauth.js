609f6b9aa820a86b283f413522679765c5abc738
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-15 15:41:06 +0900

    connect the nav login buttons to the authformpopup

--- client/components/Nav/navauth.js
+++ client/components/Nav/navauth.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Button from "../Button";
+import { useAuthPopup } from "../../contexts/authpopup";
 
-const NavAuth = () => (
-  <div className="nav__item__auth">
-    <Button cx="nav__item__auth__btn" inverted text="log in" />
-    <Button cx="nav__item__auth__btn" text="sign up" />
-  </div>
-);
+export default function NavAuth() {
+  const setAuthPopup = useAuthPopup();
 
-export default NavAuth;
+  return (
+    <div className="nav__item__auth">
+      <Button
+        cx="nav__item__auth__btn"
+        handleClick={() => setAuthPopup([true, "signin"])}
+        inverted
+        text="log in"
+      />
+      <Button
+        cx="nav__item__auth__btn"
+        handleClick={() => setAuthPopup([true, "signup"])}
+        text="sign up"
+      />
+    </div>
+  );
+}

2164af26c2575fcbda21509df6c4f3e352628d5d
beboptank <beboptank12@gmail.com>
2019-12-15 17:00:40 +0900

    added styling, image from Reddit Premium page, and premium benefits content

--- client/components/PremiumHero/premiumhero.js
+++ client/components/PremiumHero/premiumhero.js
@@ -15,6 +15,55 @@ export default function PremiumHero() {
           <Button text="Get Reddit Premium" color="orange"></Button>
         </div>
       </div>
+      <div className="subsection">
+        <h1 className="subsection--header">Join Reddit Premium Today</h1>
+        <div className="benefits">
+          <div className="benefits--info">
+            <ul className="benefits--list">
+              <li className="benefits--point">Ads-free experience</li>
+              <li className="benefits--point">700 Coins every month</li>
+            </ul>
+            <Button text="$5.99/Month" color="orange"></Button>
+            <div className="benefits--gift">
+              <p>1,000 Coins Sign Up Gift</p>
+            </div>
+            <div className="benefits--image"></div>
+          </div>
+          <div>
+            <h3>Subscription automatically renews monthly</h3>
+            <h3>
+              <a href="#">REDEEM A GIFT CODE</a>
+            </h3>
+          </div>
+          <div>
+            <h2>Reddit Premium FAQ</h2>
+          </div>
+          <div>
+            <div>
+              <h3>What is a Reddit Premium Membership?</h3>
+            </div>
+            <div>
+              <h3>
+                Why change the name to Premium? What happened to calling it
+                Gold?
+              </h3>
+            </div>
+            <div>
+              <h3>
+                What if I was subscribed to the old Gold Membership Program?
+              </h3>
+            </div>
+            <div>
+              <h3>
+                The Premium membership gives me Coins, what are those for?
+              </h3>
+            </div>
+            <div>
+              <h3>Do I have to subscribe to Reddit Premium to get Coins?</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

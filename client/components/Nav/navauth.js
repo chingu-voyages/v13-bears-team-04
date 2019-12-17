d00a9e2405fa11f2cf478a94e15c15777f382d17
unknown <danielstrong.ds@gmail.com>
2019-12-17 16:28:30 +0900

    made part of the nav dynamic based on if user is logged in

--- client/components/Nav/navauth.js
+++ client/components/Nav/navauth.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Button from "../Button";
 import { useAuthPopup } from "../../contexts/authpopup";
+import { useUser } from "../../contexts/user";
+import NavQuickLinksLink from "./navquicklinkslink";
 
 export default function NavAuth() {
+  const { user } = useUser();
   const { setAuthPopup } = useAuthPopup();
 
-  return (
+  return user ? (
+    <div className="nav__item__quicklinks">
+      <NavQuickLinksLink
+        href="/chat"
+        title="Chat"
+        icon="comment-dots"
+        color="orange"
+      />
+      <NavQuickLinksLink
+        href="/message/inbox"
+        title="Messages"
+        icon="envelope"
+      />
+      <NavQuickLinksLink
+        href="/submit"
+        title="Create Post"
+        icon="pen"
+        color="blue"
+      />
+    </div>
+  ) : (
     <div className="nav__item__auth">
       <Button
         cx="nav__item__auth__btn"

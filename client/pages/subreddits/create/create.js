84efc1193a29bb13e324fbbe454d727d8ec0fadd
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-22 15:58:55 +0900

    removed unneeded classnames from checkbox and radio

--- client/pages/subreddits/create/create.js
+++ client/pages/subreddits/create/create.js
@@ -75,7 +75,7 @@ export default function Create() {
             role="radiogroup"
           >
             <div className="subcreate__type__item">
-              <Radio cx="subcreate__type__item__radio" />
+              <Radio />
               <FontAwesomeIcon
                 className="subcreate__type__item__icon"
                 icon="user"
@@ -91,7 +91,7 @@ export default function Create() {
               </div>
             </div>
             <div className="subcreate__type__item">
-              <Radio cx="subcreate__type__item__radio" isChecked />
+              <Radio isChecked />
               <FontAwesomeIcon
                 className="subcreate__type__item__icon"
                 icon="eye"
@@ -108,7 +108,7 @@ export default function Create() {
               </div>
             </div>
             <div className="subcreate__type__item">
-              <Radio cx="subcreate__type__item__radio" isChecked />
+              <Radio isChecked />
               <FontAwesomeIcon
                 className="subcreate__type__item__icon"
                 icon="lock"
@@ -127,8 +127,8 @@ export default function Create() {
         </div>
         <div className="subcreate__adult">
           <h3 className="subcreate__header">Adult content</h3>
-          <Checkbox cx="subcreate__adult__checkbox" />
-          <Checkbox cx="subcreate__adult__checkbox" isChecked />
+          <Checkbox />
+          <Checkbox isChecked />
           {/* checkbox */}
           {/* label */}
           {/* text */}

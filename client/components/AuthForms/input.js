a22b37c7fd8677a277ed2021badfe44d43eb90a9
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-22 14:12:59 +0900

    updated Input to accept a required prop

--- client/components/AuthForms/input.js
+++ client/components/AuthForms/input.js
@@ -1,12 +1,24 @@
 import React from "react";
+import clsx from "clsx";
 
 const Input = props => {
-  const { value, handleChange, type, label, minLength, maxLength } = props;
+  const {
+    value,
+    handleChange,
+    type,
+    label,
+    minLength,
+    maxLength,
+    required,
+  } = props;
+
+  const id = `form__field__input--${label}`;
+  const labelCx = clsx("form__field__label", { "required-dot": required });
 
   return (
     <div className="form__field">
       <input
-        id={`form__field__input--${label}`}
+        id={id}
         className="form__field__input"
         type={type}
         value={value}
@@ -18,10 +30,7 @@ const Input = props => {
         autoComplete="on"
         required
       />
-      <label
-        htmlFor={`form__field__input--${label}`}
-        className="form__field__label"
-      >
+      <label htmlFor={id} className={labelCx}>
         {label}
       </label>
     </div>

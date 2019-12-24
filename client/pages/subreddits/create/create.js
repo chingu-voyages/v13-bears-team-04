a77ed1d84436c6be5b9e56d79ae85569d2474fa9
unknown <danielstrong.ds@gmail.com>
2019-12-24 13:56:59 +0900

    added state management

--- client/pages/subreddits/create/create.js
+++ client/pages/subreddits/create/create.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateAdult from "./createadult";
 import CreateFields from "./createfields";
 import CreateType from "./createtype";
@@ -6,6 +6,12 @@ import Button from "../../../components/Button";
 import "./create.scss";
 
 export default function Create() {
+  const [name, setName] = useState("");
+  const [topics, setTopics] = useState([]);
+  const [description, setDescription] = useState("");
+  const [comType, setComType] = useState("public");
+  const [isOver18, setIsOver18] = useState(false);
+
   function handleSubmit() {
     console.log("processing...");
   }
@@ -15,9 +21,16 @@ export default function Create() {
       <div className="subcreate__left" />
       <div className="subcreate__right">
         <h1 className="subcreate__header">Create a community</h1>
-        <CreateFields />
-        <CreateType checkedValue="public" />
-        <CreateAdult isChecked={false} />
+        <CreateFields
+          setName={setName}
+          setTopics={setTopics}
+          setDescription={setDescription}
+          name={name}
+          topics={topics}
+          description={description}
+        />
+        <CreateType setComType={setComType} checkedValue={comType} />
+        <CreateAdult setIsOver18={setIsOver18} isChecked={isOver18} />
         <Button
           cx="subcreate__button"
           text="Create Community"

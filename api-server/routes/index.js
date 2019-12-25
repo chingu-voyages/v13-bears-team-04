decb98cf40ff89762716093c68c056ac1a3e3ffc
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-25 11:25:51 +0900

    and again

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const posts = require("./posts");
 const users = require("./users");
-const community = require("./communities");
+const communities = require("./communities");
 
 router.get("/", (_, res) => res.send("v13 Bears 4 Reddit Clone API"));
 router.use("/user", users);
 router.use("/posts", posts);
-router.use("/community", community);
+router.use("/community", communities);
 
 module.exports = router;

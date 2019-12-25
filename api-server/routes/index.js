f3ec659b8187b1005b69b1f75ba2061648beb2d0
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-25 11:25:17 +0900

    pluralized file name to stay consistent with the folder

--- api-server/routes/index.js
+++ api-server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const posts = require("./posts");
 const users = require("./users");
-const community = require("./community");
+const community = require("./communities");
 
 router.get("/", (_, res) => res.send("v13 Bears 4 Reddit Clone API"));
 router.use("/user", users);

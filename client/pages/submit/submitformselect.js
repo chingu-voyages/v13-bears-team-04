19971dfb5568369629163f3fbef326ff88a2af5b
Daniel Strong <danielstrong.ds@gmail.com>
2019-12-29 17:06:39 +0900

    finished basic styling of Select

--- client/pages/submit/submitformselect.js
+++ client/pages/submit/submitformselect.js
@@ -1,30 +1,17 @@
 import React from "react";
 import AsyncSelect from "react-select/async";
-import fetchIt from "../../utils/fetch";
-
-const styles = {};
-
-async function getOptions() {
-  try {
-    const communities = await fetchIt("/community");
-    console.log(communities);
-    return communities.map(({ name }) => ({
-      label: name,
-      value: name,
-      key: name,
-    }));
-  } catch (err) {
-    console.log(err);
-    return [];
-  }
-}
+import styles from "./submitformselectstyles";
+import getOptions from "./submitformselectoptions";
 
 const SubmitFormSelect = () => {
   return (
     <AsyncSelect
-      cacheOptions
       isSearchable
+      isClearable
+      defaultOptions
+      cacheOptions
       loadOptions={getOptions}
+      styles={styles}
       className="submit__form__select"
       instanceId="submit__form__select"
       placeholder="Choose a community"

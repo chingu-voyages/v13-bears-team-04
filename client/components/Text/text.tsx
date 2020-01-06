7f0593eb5a0ed9bf00355dfc23644c7125998b95
unknown <danielstrong.ds@gmail.com>
2020-01-06 16:26:29 +0900

    changed js to tsx

---
+++ client/components/Text/text.tsx
@@ -0,0 +1,32 @@
+// https://docs.slatejs.org/walkthroughs/01-installing-slate
+
+import React, { useMemo, useState } from "react";
+import { createEditor, Node } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+import Toolbar from "./components/Toolbar";
+import "./text.scss";
+
+const Text = ({ isRich = true }) => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const [value, setValue] = useState<Node[]>([
+    { type: "paragraph", children: [{ text: "" }] },
+  ]);
+
+  const [showRichOptions, setShowRichOptions] = useState(isRich);
+  const toggleShowRichOptions = () => setShowRichOptions(state => !state);
+
+  return (
+    <div className="editor">
+      <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+        <Toolbar
+          showRichOptions={showRichOptions}
+          toggleShowRichOptions={toggleShowRichOptions}
+        />
+        <Editable placeholder="Text (Optional)" />
+      </Slate>
+    </div>
+  );
+};
+
+export default Text;

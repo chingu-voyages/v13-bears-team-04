02f63633b33d14b2d6c51d24609d7ddf4aef3480
Daniel Strong <danielstrong.ds@gmail.com>
2020-01-09 19:18:21 +0900

    connected submit post types to context

--- client/pages/submit/submitformbodytypes.js
+++ client/pages/submit/submitformbodytypes.js
@@ -1,28 +1,41 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import clsx from "clsx";
+import FAIcon from "../../components/FAIcon";
+import { useCreatePost } from "../../contexts/createpost";
 
+const baseCX = "submit__form__body__types__btn";
 const types = [
-  { icon: "keyboard", text: "Post" },
-  { icon: "image", text: "Image & Video" },
-  { icon: "link", text: "Link" },
+  { icon: "keyboard", text: "Post", postType: "text" },
+  { icon: "image", text: "Image & Video", postType: "file", disabled: true },
+  { icon: "link", text: "Link", postType: "link" },
 ];
 
-const SubmitFormBodyTypes = () => (
-  <div className="submit__form__body__types">
-    {types.map(({ icon, text }) => (
-      <button
-        key={text}
-        type="button"
-        className="submit__form__body__types__btn"
-      >
-        <FontAwesomeIcon
-          icon={icon}
-          className="submit__form__body__types__btn__icon"
-        />
-        {text}
-      </button>
-    ))}
-  </div>
-);
+export default function SubmitFormBodyTypes() {
+  const { state, createPostDispatch } = useCreatePost();
 
-export default SubmitFormBodyTypes;
+  return (
+    <div className="submit__form__body__types">
+      {types.map(({ icon, text, postType, disabled }) => (
+        <button
+          key={text}
+          type="button"
+          className={clsx(
+            baseCX,
+            state.postType === postType && `${baseCX}--active`,
+            disabled && `${baseCX}--disabled`
+          )}
+          onClick={() => {
+            if (disabled) return;
+            createPostDispatch({ type: "SET_TYPE", postType });
+          }}
+        >
+          <FAIcon
+            icon={icon}
+            className="submit__form__body__types__btn__icon"
+          />
+          {text}
+        </button>
+      ))}
+    </div>
+  );
+}

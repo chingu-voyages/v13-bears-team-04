bd19b53b875abe0015b22ce80e33c8cd2a041ebb
beboptank <beboptank12@gmail.com>
2020-01-10 21:57:11 +0900

    added dummy info to state

--- client/components/RulesBox/rulesbox.js
+++ client/components/RulesBox/rulesbox.js
@@ -4,6 +4,12 @@ import "../Button/";
 import "./rulesbox.scss";
 
 export default function RulesBox() {
+  const [rules] = useState({
+    rule1: "1. Be nice",
+    rule2: "2. Be cool",
+    rule3: "3. Be Bueller",
+  });
+
   return (
     <div>
       <h1>Rules!</h1>

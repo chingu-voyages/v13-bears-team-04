dc4e5daa9f995333f128341bde47bac9e89901b8
unknown <danielstrong.ds@gmail.com>
2020-01-10 15:10:55 +0900

    connected form content to context and passed down to Text

--- client/pages/submit/submitformbodycontent.js
+++ client/pages/submit/submitformbodycontent.js
@@ -1,14 +1,21 @@
 import React from "react";
 import SubmitFormBodyContentTitle from "./submitformbodycontenttitle";
 import Text from "../../components/Text";
+import { useCreatePost } from "../../contexts/createpost";
 
-const SubmitFormBodyContent = () => {
+export default function SubmitFormBodyContent() {
+  const { state, createPostDispatch } = useCreatePost();
+
   return (
     <div className="submit__form__body__content">
       <SubmitFormBodyContentTitle />
-      <Text />
+      <Text
+        activeOptions={state.activeOptions}
+        value={state.content}
+        setValue={val => {
+          createPostDispatch({ type: "SET_CONTENT", content: val });
+        }}
+      />
     </div>
   );
-};
-
-export default SubmitFormBodyContent;
+}

6c89c62e4f3ae6d80a98bdfdd1622f532236bc61
beboptank <beboptank12@gmail.com>
2020-01-10 21:50:32 +0900

    added dummy content to allow commit

---
+++ client/components/RulesBox/rulesbox.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Button from "../Button";
+import "../Button/";
+import "./rulesbox.scss";
+
+export default function RulesBox() {
+  return (
+    <div>
+      <h1>Rules!</h1>
+    </div>
+  );
+}

50ffb8a5d53b0a0e0effc7acb52efbc69c39f16c
Daniel Strong <danielstrong.ds@gmail.com>
2020-01-12 16:04:10 +0900

    finished/moved/renamed Editor component

--- client/pages/submit/submitformbodycontent.js
+++ client/pages/submit/submitformbodycontent.js
@@ -1,27 +1,23 @@
 import React from "react";
+import Editor from "../../components/Editor";
 import SubmitFormBodyContentTitle from "./submitformbodycontenttitle";
-import CustomEditor from "../../components/CustomEditor";
 import { useCreatePost } from "../../contexts/createpost";
-import { useIsDesktop } from "../../hooks";
 
 export default function SubmitFormBodyContent({ isUserBrowser }) {
   const { state, createPostDispatch } = useCreatePost();
-  const isDesktop = useIsDesktop(isUserBrowser ? 1200 : 450);
+
+  const setValue = value => {
+    createPostDispatch({ type: "SET_CONTENT", content: value });
+  };
 
   return (
     <div className="submit__form__body__content">
       <SubmitFormBodyContentTitle />
-      {/* we only want to show the rich text editor on desktop */}
-      {isDesktop ? (
-        <CustomEditor
-          value={state.content}
-          setValue={val => {
-            createPostDispatch({ type: "SET_CONTENT", content: val });
-          }}
-        />
-      ) : (
-        <textarea name="sds" id="sds" cols="30" rows="10"></textarea>
-      )}
+      <Editor
+        isBrowser={isUserBrowser}
+        value={state.content}
+        setValue={setValue}
+      />
     </div>
   );
 }

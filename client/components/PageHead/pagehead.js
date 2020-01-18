4c0398697e6537d7ffaab2be00e5e9e967a874d1
beboptank <beboptank12@gmail.com>
2020-01-19 08:42:57 +0900

    added initial next.js Head tag and title, meta description tags

--- client/components/PageHead/pagehead.js
+++ client/components/PageHead/pagehead.js
@@ -1,2 +1,11 @@
 import React from "react";
 import Head from "next/head";
+
+export default function PageHead(props) {
+  return (
+    <Head>
+      <title>{props.title}</title>
+      <meta name="description" content={props.description} />
+    </Head>
+  );
+}

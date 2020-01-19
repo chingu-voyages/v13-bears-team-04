0692ca639c5e0d73cbb6d1321357cce0f92e6e85
beboptank <beboptank12@gmail.com>
2020-01-19 13:33:43 +0900

    added meta tags and default props

--- client/components/PageHead/pagehead.js
+++ client/components/PageHead/pagehead.js
@@ -6,6 +6,15 @@ export default function PageHead(props) {
     <Head>
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
+      <link rel="canonical" href={`${props.canonical}`} />
     </Head>
   );
 }
+
+PageHead.defaultProps = {
+  title: "reddit: the front page of the internet",
+  description: `Reddit is a network of communities based on people's interests. 
+    Find communities you're interested in, and become a part of an online
+    community!`,
+  url: "www.reddit.com/page",
+};

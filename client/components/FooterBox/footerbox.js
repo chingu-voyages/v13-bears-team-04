3e196c5942abbfa193ec5c2f718580febf184eed
beboptank <beboptank12@gmail.com>
2020-01-21 20:03:36 +0900

    style footer box

--- client/components/FooterBox/footerbox.js
+++ client/components/FooterBox/footerbox.js
@@ -3,98 +3,102 @@ import "./footerbox.scss";
 
 export default function FooterBox() {
   return (
-    <div className="footerboxcontainer">
+    <div className="footerbox">
       <div>
-        <ul className="footerboxlist redditlist">
-          <h2>Reddit</h2>
+        <ul className="footerbox__list">
           <li>
-            <a className="footerboxlink" href="#">
-              About
-            </a>
-          </li>
-          <li>
-            <a className="footerboxlink" href="#">
-              Careers
-            </a>
-          </li>
-          <li>
-            <a className="footerboxlink" href="#">
-              Press
-            </a>
-          </li>
-          <li>
-            <a className="footerboxlink" href="#">
-              Advertise
-            </a>
-          </li>
-          <li>
-            <a className="footerboxlink" href="#">
-              Blog
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul className="footerboxlist">
-          <h2>Using Reddit</h2>
-          <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Help
             </a>
           </li>
           <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Reddit App
             </a>
           </li>
           <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Reddit Coins
             </a>
           </li>
           <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Reddit Premium
             </a>
           </li>
           <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Reddit Gifts
             </a>
           </li>
           <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Communities
             </a>
           </li>
           <li>
-            <a className="footerboxlink" href="#">
+            <a className="footerbox__link" href="#">
               Top Posts
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Topics
             </a>
           </li>
         </ul>
       </div>
       <div>
-        <p>
-          <a className="footerboxlink" href="#">
-            Terms
-          </a>{" "}
-          |{" "}
-          <a className="footerboxlink" href="#">
-            Content Policy
-          </a>{" "}
-          |{" "}
-          <a className="footerboxlink" href="#">
-            Privacy Policy
-          </a>{" "}
-          |{" "}
-          <a className="footerboxlink" href="#">
-            Mod Policy
-          </a>
-        </p>
+        <ul className="footerbox__list">
+          <li>
+            <a className="footerbox__link" href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Careers
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Press
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Advertise
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Terms
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Content Policy
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a className="footerbox__link" href="#">
+              Mod Policy
+            </a>
+          </li>
+        </ul>
       </div>
-      <div>
-        <p>Definitely Not Reddit Inc &copy; 2019</p>
+      <div className="footerbox__copyright">
+        <p>Definitely Not Reddit Inc &copy; 2020</p>
       </div>
     </div>
   );

dd0df6043e73cff39811ca1af639f4814fe1b73c
Daniel Strong <danielstrong.ds@gmail.com>
2020-01-21 10:28:03 +0900

    switch uselayouteffect to useeffect

--- client/hooks/useSetCSSVariable.tsx
+++ client/hooks/useSetCSSVariable.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useLayoutEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 type Key = string;
 type Prop = string;
@@ -10,7 +10,7 @@ export default function useSetCSSVariable(key: Key | Keys, property?: Prop) {
     []
   );
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (typeof key === "string") return setter(key, property);
     Object.keys(key).map(k => setter(k, key[k]));
   }, [key, property]);

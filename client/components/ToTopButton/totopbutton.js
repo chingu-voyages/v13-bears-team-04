d59a1d98a83001e434caecaf3c2923f36cd94039
Saggitarie <saggitarie@gmail.com>
2020-01-22 18:26:19 +0900

    ToTopButton component refactored

--- client/components/ToTopButton/totopbutton.js
+++ client/components/ToTopButton/totopbutton.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Button from "../Button";
-import "./totopbutton.scss";
+// import "./totopbutton.scss";
 
 export default function ToTopButton() {
   const [showButton, setShowButton] = useState(false);

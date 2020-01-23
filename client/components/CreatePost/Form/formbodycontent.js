f54e87eac43847b9f0f0071fe7a7f187d3908e2b
Daniel Strong <danielstrong.ds@gmail.com>
2020-01-23 16:22:30 +0900

    renamed Editor to Slate since it holds two exports now and updated import paths

--- client/components/CreatePost/Form/formbodycontent.js
+++ client/components/CreatePost/Form/formbodycontent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Editor } from "../../Editor";
+import { Editor } from "../../Slate";
 import SubmitFormBodyContentTitle from "./formbodycontenttitle";
 import { useCreatePost } from "../../../contexts/createpost";
 

a39039edf4e51eb6757f52d7cddb2d3b5f48918c
beboptank <beboptank12@gmail.com>
2020-01-25 17:31:05 +0900

    import subreddit info styles, restyle to use layout component, remove auth context until later

--- client/components/SubredditInfo/subredditinfo.js
+++ client/components/SubredditInfo/subredditinfo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+import "../../sass/components/_subredditinfo.scss";
 import Button from "../Button/button";
 
 export default function SubredditInfo() {

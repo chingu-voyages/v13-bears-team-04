28e1308c0319b7d18436e065a444190b136cb9cd
beboptank <beboptank12@gmail.com>
2020-01-27 23:13:34 +0900

    add useState hook and add state

--- client/components/Post/post.js
+++ client/components/Post/post.js
@@ -1,1 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "../../sass/components/_post.scss";
+
+export default function Post() {
+  const [postInfo] = useState({
+    user: "heyoheyo",
+    content: "content here",
+    likes: 0 + "k",
+    subreddit: "r/coolsubreddit",
+    numOfComments: 0 + "k",
+  });
+}

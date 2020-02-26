413f46a8a0115734ae1063cb8186375895bc04ce
unknown <danielstrong.ds@gmail.com>
2020-02-26 13:48:52 +0700

    export all model from index file

---
+++ api-server/models/index.js
@@ -0,0 +1,9 @@
+import Comment from "./comment";
+import Community from "./community";
+import Post from "./post";
+import Session from "./session";
+import Topic from "./topic";
+import User from "./user";
+import Vote from "./vote";
+
+export { Comment, Community, Post, Session, Topic, User, Vote };
